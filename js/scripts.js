efe261528de8bbf0d0ac1a7b4024cc1e3f930752
Rita Bennett-Chew and Will Swanson <will@>
2018-05-30 15:29:36 -0700

    add two numbers and not break

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
+// Business logic:
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -11,13 +9,20 @@ var subtract = function(number1, number2) {
 
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
+};
 
 var divide = function(number1, number2){
   return number1 / number2;
-}
+};
 
-alert("Addition: " + add(number1, number2));
-alert("Subtraction: " + subtract(number1, number2));
-alert("Multiplication: " + multiply(number1, number2));
-alert("Division: " + divide(number1, number2));
+// User interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  })
+});
