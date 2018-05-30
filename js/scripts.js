a8bc94c72ea28b83b9bb127e646a4ea3098dc43d
Rita Bennett-Chew and Will Swanson <rita@>
2018-05-30 16:04:35 -0700

    add 3 other calcs for subtract multiply and divide and associated js code

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,35 @@ var divide = function(number1, number2){
 // User interface logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#addition").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
-  })
+    $("#output1").text(result);
+  });
+
+  $("form#subtraction").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+
+  $("form#multiplication").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+
+  $("form#division").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
 });
